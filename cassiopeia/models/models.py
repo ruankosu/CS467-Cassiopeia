1b345cb2387fac1b44bdd8d95ceb5c05c76664a8
ellisken <ellisken@oregonstate.edu>
2018-07-09 14:26:20 -0700

    Started db definition

--- cassiopeia/models/models.py
+++ cassiopeia/models/models.py
@@ -2,6 +2,73 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# User
 class User(db.Model):
-    #fields here
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    # I plan on creating a function for password hashing
+    password = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    native_language = db.Relationship('Language', backref='language' , lazy=True)
 
+    def __repr__(self):
+        return f"User('{self.username}', '{self.first_name}', '{self.last_name}', '{self.email}')"
+
+
+# Country
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True, nullable=False)
+    # Need to set default flag image
+    flag_image = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+
+# Category
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True, nullable=False)
+
+
+# Language
+class Language(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True, nullable=False)
+
+
+# Locale
+class Locale(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=False)
+
+# User_Language_Skill
+class User_Language_Skill(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    language_id = db.Column(db.Integer, db.ForeignKey('language.id'), nullable=False)
+
+
+# Content
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), nullable=False)
+    language_id = db.Column()
+    pub_date
+    url
+    level
+
+
+# Content_Category
+class Content_Category(db.Model):
+    content_id
+    category_id
+
+
+# Progress
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id
+    content_id
+    read_date
+    rating
+    read_ct
