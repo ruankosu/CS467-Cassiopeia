1735395fa90b6e2dfe48c99ada6f6d48dc6f02bc
ellisken <ellisken@oregonstate.edu>
2018-07-10 09:51:49 -0700

    Made changes to database lines

--- cassiopeia/__init__.py
+++ cassiopeia/__init__.py
@@ -1,14 +1,16 @@
 import os
 from flask import Flask
+#from cassiopeia.config import Config
 
-def create_app(test_config=None):
+def create_app(test_class=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
+    #app.config.from_object(Config)
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
         SECRET_KEY='SuperSecret9000',
         # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SQLALCHEMY_DATABASE_URI=os.path.join(app.instance_path, 'sqlite:///site_test.db'),
     )
 
     if test_config is None:
@@ -29,8 +31,9 @@ def create_app(test_config=None):
     #     return 'Hello, World!'
 
     # register the database commands
-    from cassiopeia import db
-    db.init_app(app)
+    from cassiopeia.models.models import db
+    with app.app_context():
+        db.init_app(app)
 
     # apply the blueprints to the app
     from cassiopeia.views import auth, content
