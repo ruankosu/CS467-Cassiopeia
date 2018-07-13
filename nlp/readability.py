dbc0726e698252da328f20af1ed9c60e9bb2babd
ellisken <ellisken@oregonstate.edu>
2018-07-13 12:26:14 -0700

    Started additional index functions

--- nlp/readability.py
+++ nlp/readability.py
@@ -22,26 +22,65 @@ def count_words(text):
 	space_count = len(filter(functools.partial(operator.contains, spaces), text))
 	return space_count
 
+def is_miniword(word):
+        # strip leading and trailing punctuation/non-alpha chars
+        word.strip('.,!?-*();:\'\"[]{}\\')
+        # finds word length
+        length = len(word)
+        # very correct length and alpha only
+        if length > 0 and length < 4:
+            if(word.isalpha()):
+                return True
+        else:
+            return False
+
+def count_miniwords(text):
+        # map(is_miniword) update count
+        miniword_count = len(filter(is_miniword, text.split()))
+        return miniword_count
+
+def count_syllables(text):
+        # count number of syllables
+        return
+
 def count_sentences(text):
-	# count sentence
+	# count sentences
 	sentenceEnd = '.!?'
 	sentence_count = len(filter(functools.partial(operator.contains, sentenceEnd), text))
 	return sentence_count
 
-
-def cal_diff(text):
+# Calculates the Automated Readability Index
+def cal_ari(text):
 	#4.71 * (characters / words) + 0.5 * (words / sentences) - 21.43
 	num_chars = count_chars(text)
 	num_words = count_words(text)
 	num_sentences = count_sentences(text)
-	diff_score = 4.71 * num_chars / num_words + 0.5 * num_words / num_sentences - 21.43
-	return diff_score
+	ari_score = 4.71 * num_chars / num_words + 0.5 * num_words / num_sentences - 21.43
+	return ari_score
 
+# Calculates the Flesch-Kincaid Index
+def cal_fki(text):
+	#4.71 * (characters / words) + 0.5 * (words / sentences) - 21.43
+	num_chars = count_chars(text)
+	num_words = count_words(text)
+	num_sentences = count_sentences(text)
+	fki_score = 4.71 * num_chars / num_words + 0.5 * num_words / num_sentences - 21.43
+	return fki_score
 
+# Calculates the McAlpine EFLAW Index (designed for ESL learners)
+def cal_mei(text):
+	#4.71 * (characters / words) + 0.5 * (words / sentences) - 21.43
+	num_chars = count_chars(text)
+	num_words = count_words(text)
+	num_sentences = count_sentences(text)
+	mei_score = 4.71 * num_chars / num_words + 0.5 * num_words / num_sentences - 21.43
+	return mei_score
+
+# Main
 if __name__ == "__main__":
-	f = open (sys.argv[1], "r")
-	if f.mode == 'r':
-		text = f.read()
-	print(cal_diff(text))
+        f = open (sys.argv[1], "r")
+        if f.mode == 'r':
+               text = f.read()
+        #print(cal_ari(text))
 
 
