0bd005861d79c4cd8da4f6c3256ddc5446a815de
ellisken <ellisken@oregonstate.edu>
2018-07-14 14:51:46 -0700

    Finished additional index calcs

--- nlp/readability.py
+++ nlp/readability.py
@@ -5,6 +5,12 @@ import re #regular expression library
 import sys
 import operator
 import functools
+import nltk
+import curses
+from curses.ascii import isdigit
+from nltk.corpus import cmudict
+#nltk.download('cmudict')
+d = cmudict.dict()
 
 
 def count_chars(text):
@@ -24,12 +30,12 @@ def count_words(text):
 
 def is_miniword(word):
         # strip leading and trailing punctuation/non-alpha chars
-        word.strip('.,!?-*();:\'\"[]{}\\')
+        word = word.strip('.,!?-*();:\'\"[]{}\\')
         # finds word length
         length = len(word)
         # very correct length and alpha only
         if length > 0 and length < 4:
-            if(word.isalpha()):
+            if word.isalpha():
                 return True
         else:
             return False
@@ -41,7 +47,14 @@ def count_miniwords(text):
 
 def count_syllables(text):
         # count number of syllables
-        return
+        wordlist = text.split()
+        syllable_count = 0
+        for word in wordlist:
+            word = word.strip('.,!?-*();:\'\"[]{}\\')
+            # checks if word is alpha && in the dictionary (prevents key errors)
+            if word.isalpha() and word in d:
+                syllable_count += [len(list(y for y in x if isdigit(y[-1]))) for x in d[word.lower()]][0]
+        return syllable_count
 
 def count_sentences(text):
 	# count sentences
@@ -60,20 +73,20 @@ def cal_ari(text):
 
 # Calculates the Flesch-Kincaid Index
 def cal_fki(text):
-	#4.71 * (characters / words) + 0.5 * (words / sentences) - 21.43
-	num_chars = count_chars(text)
+	# 206.835 - 1.015 * (words / sentences) - 84.6 * (syllables / words)
 	num_words = count_words(text)
 	num_sentences = count_sentences(text)
-	fki_score = 4.71 * num_chars / num_words + 0.5 * num_words / num_sentences - 21.43
+        num_syllables = count_syllables(text)
+	fki_score = 206.835 = 1.015 * num_words / num_sentences - 84.6 * num_syllables / num_words
 	return fki_score
 
 # Calculates the McAlpine EFLAW Index (designed for ESL learners)
 def cal_mei(text):
-	#4.71 * (characters / words) + 0.5 * (words / sentences) - 21.43
-	num_chars = count_chars(text)
+	#words + miniwords / sentences
 	num_words = count_words(text)
-	num_sentences = count_sentences(text)
-	mei_score = 4.71 * num_chars / num_words + 0.5 * num_words / num_sentences - 21.43
+	num_miniwords = count_miniwords(text)
+        num_sentences = count_sentences(text)
+	mei_score = num_words + num_miniwords / num_sentences
 	return mei_score
 
 # Main
@@ -81,6 +94,8 @@ if __name__ == "__main__":
         f = open (sys.argv[1], "r")
         if f.mode == 'r':
                text = f.read()
-        #print(cal_ari(text))
+        print(cal_ari(text))
+        #print(cal_fki(text))
+        #print(cal_mei(text))
 
 
