faa1941afbd9bcb913a0b16d5957d66df645b11b
ellisken <ellisken@oregonstate.edu>
2018-07-17 09:50:27 -0700

    Updated register route

--- cassiopeia/views/auth.py
+++ cassiopeia/views/auth.py
@@ -1,10 +1,12 @@
 import functools
 import os
+import sys
 from flask import (
         Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from cassiopeia.models.models import db
 from cassiopeia.models.models import User
+#from cassiopeia import bcrypt
 #from cassiopeia import db
 
 # Register database context
@@ -17,36 +19,41 @@ auth = Blueprint('auth', __name__)
 
 
 # Routes
-@auth.route("/register", methods=['POST'])
+@auth.route("/register", methods=['GET', 'POST'])
 def register():
-    # Confirm username is unique
-    username = request.form['username']
-    already_taken = User.query.filter_by(username).first()
-    if already_taken:
-        flash('Username already in use. Please choose a different one.', 'error')
-        #return redirect(url_for(''))
+    if request.method == 'POST':
+        '''# Confirm username is unique
+        username = request.form['username']
+        already_taken = User.query.filter_by(username=username).first()
+        if already_taken:
+            flash('Username already in use. Please choose a different one.', 'error')
+            #return redirect(url_for(''))
 
-    # Confirm email is unique
-    email = request.form['inputEmail']
-    email_in_use = User.query.filter_by(email).first()
-    if email_in_use:
-        flash('Email already associated with an account. Please log in or use a different email address.', 'error')
-        #return redirect(url_for(''))
+        # Confirm email is unique
+        email = request.form['inputEmail']
+        email_in_use = User.query.filter_by(email).first()
+        if email_in_use:
+            flash('Email already associated with an account. Please log in or use a different email address.', 'error')
+            #return redirect(url_for(''))
 
-    # Confirm registration password matches confirmation password
-    # If password != confirmation password, flash error
-    password = request.form('inputPassword')
-    if password != request.form('confirmPassword'):
-        flash('Passwords entered do not match. Please correct and resubmit.', 'error')
-        # redirect?
-
-    hashed_pwd = bcrypt.generate_password_hash(request.form('inputPassword')).decode('utf-8')
-    new_user = User(username=request.form('username'), email=request.form('inputEmail'), password=hashed_pwd)
-    db.session.add(new_user)
-    db.session.commit()
-    flash('Your account has been created! You are now able to log in', 'success')
-    return redirect(url_for('auth.login'))
-    return render_template('signup.html', title='Sign Up')
+        # Confirm registration password matches confirmation password
+        # If password != confirmation password, flash error
+        password = request.form['inputPassword']
+        if password != request.form['confirmPassword']:
+            flash('Passwords entered do not match. Please correct and resubmit.', 'error')
+            # redirect?'''
+        email = request.form['inputEmail']
+        username = request.form['username']
+        password = request.form['inputPassword']
+        print(email+' '+username+' '+password, file=sys.stderr)
+        #hashed_pwd = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        print("Committed to db successfully", file=sys.stderr)
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('signup/signup.html', title='Sign Up')
 
 
 @auth.route("/login", methods=['GET', 'POST'])
