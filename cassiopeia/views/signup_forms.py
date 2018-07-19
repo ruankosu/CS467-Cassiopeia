9377bf682935701b4c71d58f0890f9c5c0d24c20
ellisken <ellisken@oregonstate.edu>
2018-07-18 22:30:36 -0700

    Changed FlaskForm to Form for validate_on_submit()

--- cassiopeia/views/signup_forms.py
+++ cassiopeia/views/signup_forms.py
@@ -1,19 +1,19 @@
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
 from cassiopeia.models.models import User
 
-class RegistrationForm(FlaskForm):
+class RegistrationForm(Form):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+            validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Username"})
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+                        validators=[DataRequired(), Email()], render_kw={"placeholder": "Email"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Create My Account')
+                                     validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Confirm Password"})
+    submit = SubmitField('Create My Account', render_kw={"placeholder": "Create My Account"})
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
@@ -26,7 +26,7 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('That email is taken. Please choose a different one.')
 
 
-class LoginForm(FlaskForm):
+class LoginForm(Form):
     email = StringField('Email address',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
