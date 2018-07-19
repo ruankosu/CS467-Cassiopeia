e16d52c4dd86c466df62c7fa2c11440e72133681
ellisken <ellisken@oregonstate.edu>
2018-07-19 11:24:38 -0700

    Added placeholders for login form

--- cassiopeia/views/signup_forms.py
+++ cassiopeia/views/signup_forms.py
@@ -1,11 +1,11 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
 from cassiopeia.models.models import User
 
-class RegistrationForm(Form):
+class RegistrationForm(FlaskForm):
     username = StringField('Username',
             validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Username"})
     email = StringField('Email',
@@ -26,9 +26,9 @@ class RegistrationForm(Form):
             raise ValidationError('That email is taken. Please choose a different one.')
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField('Email address',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+            validators=[DataRequired(), Email()], render_kw={"placeholder": "Email address"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
