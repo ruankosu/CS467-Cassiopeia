737c8f8041abcd55b3d248ebf0480a7662216d29
Kevin Ruan <ruank@oregonstate.edu>
2018-07-28 22:50:58 -0700

    Add Vue and Articles API

--- cassiopeia/static/content.js
+++ cassiopeia/static/content.js
@@ -1,5 +1,10 @@
+// jQuery functions
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
 });
+
+// Vue Components and functions, API call with Axios
+
+
