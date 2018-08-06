ec8665c1e926939f1184a235a70429cda0e7e7b7
Kevin Ruan <ruank@oregonstate.edu>
2018-08-05 21:27:20 -0700

    Frontend work: Work on routes for Language and History Add modal reader for articles Add functional rating system and connect to the backend Add feedback toaster for rating system

--- cassiopeia/api.py
+++ cassiopeia/api.py
@@ -1,9 +1,10 @@
 # Article API 
+import json
 from datetime import datetime
-from cassiopeia.models.models import User, Content, Language
+from cassiopeia.models.models import User, Content, Language, Progress
 from cassiopeia import db
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app, json
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
 )
 
 # Helps handle user sessions
@@ -21,11 +22,21 @@ def response_wrapper(data, status):
   return response
 
 # Convert contents to Python dictionary
-def contents_to_obj(contents):
+def contents_to_obj(contents, user_info=None, languages=None, categories=None):
   payload = {}
   content_arr = []
   last_id = -1
 
+  # Add user info
+  if user_info is not None:
+    payload['user_info'] = {
+      "username" : user_info["user"].username, 
+      "email" : user_info["user"].email,
+      "language": user_info["language"],
+      "category": user_info["category"]
+    }
+
+  # Add content
   for c in contents:
     obj = {}
     obj["id"] = c.id
@@ -38,8 +49,15 @@ def contents_to_obj(contents):
     content_arr.append(obj)
     last_id = c.id
 
+  # Add pagination and contents
   payload['last_id'] = last_id
   payload['contents'] = content_arr
+  
+  # Add language and categories
+  if languages is not None and categories is not None:
+    payload['user_languages'] = languages
+    payload['user_categories'] = categories
+
   return payload
 
 # ----------------------------- Article APIs ----------------------------------- #
@@ -54,48 +72,141 @@ def getArticlePages():
       return response_wrapper(data, 200)
   return response_wrapper({"error": "Something went wrong"}, 500)
 
-# Get articles with pagination 
+# Get articles with pagination, default 20 articles provided based on first language and category
+# selected. There should be more work later to make it more flexible. i.e. return all Sports categories
+#
 # Query String:
 # page - page number (int)
+# language - String
+# category - String
 # last_id - last seen id (int)
 # direction - paging direction (next, prev)
 @api.route("/article", methods=['GET'])
 @login_required
 def getArticle():  
   try:
-    mysql = db.get_db() # Establish database connection
+    db.get_db() # Establish database connection
+
+    # Get param values
     page = request.args.get('page')
     language = request.args.get('language')
+    category = request.args.get('category')
     last_id = request.args.get('last_id')
     direction = request.args.get('dir')
-    skill = None
+
+    # Get user 
+    user_info = {}
+    user = User.query.filter(User.id == current_user.id).first()
+
+    # Get user languages and categories
+    user_languages = []
+    user_categories = [{"name": c.name, "selected": 0} for c in user.categories]
 
     # Get user level for selected language
-    user = User.query.filter(User.id == current_user.id).first()
-    for l in user.languages:
-      selected_lang = Language.query.filter(Language.iso639_2 == str(language)).first()
-      if selected_lang is not None:
-        skill = l.skill
+    for l in iter(user.languages):
+      selected = 0
+      l_item = Language.query.filter(Language.id == l.language_id).first()
+      if language is not None:
+        if Language.query.filter(l_item.iso639_2 == str(language)).first():
+          selected = 1
+      user_languages.append({"language_name": l_item.name, "iso": l_item.iso639_2, "skill": l.skill, "selected": selected})
+
+    # Finalize default language
+    language = list(filter(lambda x: x["selected"] == 1, user_languages))
+    if len(language) == 0:
+      language = user_languages[0]
+      language["selected"] = 1
+    else:
+      language = language[0]
+
+    # Set Default category
+    if category is None:
+      user_categories[0]["selected"] = 1
+      category = user_categories[0]
+
+    # Set user_info
+    user_info["user"] = user
+    user_info["language"] = language["iso"]
+    user_info["category"] = category["name"]
 
     # return first 20
     if int(page) == 1 and last_id is None and direction is None:
-      contents = Content.query.filter(Content.language.has(Language.iso639_2 == language), Content.level <= skill).limit(20).all()
-      return response_wrapper(contents_to_obj(contents), 200)
-
+      contents = Content.query.filter(Content.language.has(Language.iso639_2 == language["iso"]), Content.level <= language["skill"]).limit(20).all()
+      return response_wrapper(contents_to_obj(contents, user_info, user_languages, user_categories), 200)
     # return next 20 based on last_id
     if direction == "next":
-      contents = Content.query.filter(Content.language.has(Language.iso639_2 == language), Content.level <= skill, Content.id > last_id).order_by(Content.id.asc()).limit(20)
-      
+      contents = Content.query.filter(Content.language.has(Language.iso639_2 == language["iso"]), Content.level <= language["skill"], Content.id > last_id).order_by(Content.id.asc()).limit(20)     
     # return prev 20 based on last_id
     if direction == "prev":
-      contents = Content.query.filter(Content.language.has(Language.iso639_2 == language), Content.level <= skill, Content.id < last_id).order_by(Content.id.asc()).limit(20)
+      contents = Content.query.filter(Content.language.has(Language.iso639_2 == language["iso"]), Content.level <= language["skill"], Content.id < last_id).order_by(Content.id.asc()).limit(20)
     
-    return response_wrapper(contents_to_obj(contents), 200)
+    return response_wrapper(contents_to_obj(contents, user_info), 200)
   except Exception as ex:
     return response_wrapper({"error": str(ex)}, 500)
 
   return response_wrapper({"error": "Something went wrong"}, 500)
 
+# API to handle article ratings made by the user. This will feed into the Progress table.
+# The value in this table will be used for NLP and Naive Bayes learning to update the 
+# user's language skill level. 
+@api.route("/article/rate", methods=['POST'])
+@login_required
+def rateArticle():
+  try:
+    mysql = db.get_db() # Establish db connection
+
+    # Get user from database
+    user = User.query.filter(User.id == current_user.id).first()
+
+    # Get data from request body
+    data = json.loads(request.data)
+    content_id = data["content_id"]
+    rating = data["rating"]
+
+    # Get progress object and set new data
+    progress = Progress(user_id=user.id, content_id=content_id, rating=rating, read_ct=1)
+
+    # Add to session and commit
+    mysql.session.add(progress)
+    mysql.session.commit()
+
+    # Return empty object with code 200
+    return response_wrapper({}, 200)
+
+  except Exception as ex:
+    return response_wrapper({"error": str(ex)}, 500)
+
+  return response_wrapper({"error": "Something went wrong"}, 500)
+
+# ----------------------------- Other APIs ----------------------------------- #
+@api.route("/history", methods=['GET'])
+@login_required
+def getHistory():
+  try:
+    history = []
+    rating_map = ["Just Right", "Too Hard", "Too Easy"]
+
+    db.get_db()
+    user = User.query.filter(User.id == current_user.id).first()
+    progress = Progress.query.filter(Progress.user_id == user.id).all()
+
+    for item in progress:
+      obj = {}
+      content = Content.query.filter(Content.id == item.content_id).first()
+      obj["name"] = str(content.name.decode("utf-8"))
+      obj["read_date"] = datetime.strftime(item.read_date,"%b %d %Y %H:%M:%S")
+      obj["rating"] = rating_map[item.rating]
+      history.append(obj)    
+
+    return response_wrapper(history, 200)
+
+  except Exception as ex:
+    return response_wrapper({"error": str(ex)}, 500)
+
+  return response_wrapper({"error": "Something went wrong"}, 500)
+
+
+# ----------------------------- Request Helper  ----------------------------------- #
 @api.after_request
 def after_request(response):
   # response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8080')
@@ -103,15 +214,3 @@ def after_request(response):
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')
   return response
-
-# ----------------------------- Other APIs ----------------------------------- #
-
-
-
-    
-
-
-
-
-
-
--- cassiopeia/static/content.js
+++ cassiopeia/static/content.js
@@ -1,11 +1,89 @@
 var baseUrl = 'http://127.0.0.1:8000'
 const EventBus = new Vue();
 
+Vue.component('modal', {
+  template: `
+    <transition name="modal">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+
+            <div class="modal-header">
+              <slot name="header">
+                default header
+              </slot>
+              <button type="button" class="close" @click="$emit('close')" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
+            <div class="modal-body">
+              <slot name="body">
+                default body
+              </slot>
+            </div>
+
+            <div class="modal-footer">
+              <slot name="footer">
+                <button type="button" class="btn btn-secondary" @click="$emit('close')">Close</button>
+              </slot>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+  `
+})
+
+Vue.component('content-sidebar', {
+  props: ['settings'],
+  methods: {
+    languageSelected: function(value) {
+      this.$root.$emit('languageSelected', value);
+    },
+    categorySelected: function(value) {
+      this.$root.$emit('categorySelected', value);
+    },
+    historySelected: function() {
+      this.$root.$emit('historySelected');
+    }
+  },
+  template: `
+    <!-- Sidebar  -->
+    <nav id="sidebar">
+        <ul class="list-unstyled components">
+            <p>{{ settings.user_info.username }}</p>
+            <li class="active">
+                <a href="#categorySubmenu" data-toggle="collapse" aria-expanded="false" aria-controls="categorySubmenu" class="dropdown-toggle">Categories</a>
+                <ul class="collapse list-unstyled" id="categorySubmenu">
+                  <li v-for="category in settings.user_categories" :key="category.name" v-on:click="categorySelected(category.name)">
+                    <a href="#">{{ category.name }}</a>
+                  </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#languageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Languages</a>
+                <ul class="collapse list-unstyled" id="languageSubmenu">
+                  <li v-for="language in settings.user_languages" :key="language.iso" v-on:click="languageSelected(language.iso)">
+                    <a href="#">{{ language.language_name }}</a>
+                  </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" v-on:click="historySelected()">History</a>
+            </li>
+            <li>
+                <a href="#">Settings</a>
+            </li>
+        </ul>
+    </nav>
+  `
+});
+
 Vue.component('article-paginator', {
   props: ['settings'],
   methods: {
     next: function() {
-      console.log("Emitting!")
       this.$root.$emit('next');
     },
     prev: function(event) {
@@ -28,7 +106,7 @@ Vue.component('article-paginator', {
 
 // Vue Components and functions, API call with Axios
 new Vue({
-  el: '#app-content',
+  el: '#app',
   data () {
     return {
       info: null,
@@ -36,16 +114,76 @@ new Vue({
       last_id: null,
       errored: false,
       first_page: false,
-      page_number: 0
+      page_number: 0,
+      showHistory: false,
+      showModal: false,
+      snackbarData: {
+        showSnackbar: false,
+        rating: null
+      },
       // groceryList: null
+      modalData: {
+        url: "",
+        name: "",
+        body: ""
+      },
+      userData: {
+        user_info: {username: null, email: null, language: null, category: null},
+        user_categories: null,
+        user_languages: null
+      }
     }
   },
   methods: {
+    articleRated: function(content_id, rating, event) {
+      var score = 0;
+      switch(rating) {
+        case "easy":
+          score = -1;
+          break;
+        case "hard":
+          score = 1;
+          break;
+        case "same":
+          score = 0
+        default:
+          score = 0;
+          break;
+      }
+      axios
+      .post(baseUrl + '/api/article/rate', {
+        content_id: content_id,
+        rating: score
+      })
+      .then(response => {
+        this.snackbarData.showSnackbar = true;
+      })
+      .catch(error => {
+        this.errored = true;
+      })
+      .finally(() => setTimeout(() => this.snackbarData.showSnackbar = false, 1500));
+    },
+    getHistory: function() {
+      this.loading = true; 
+      this.showHistory = false;
+      axios
+      .get(baseUrl + '/api/history', {withCredentials: true})
+      .then(response => {
+        this.history = response.data;
+      })
+      .catch(error => {
+        this.errored = true;
+      })
+      .finally(() => { 
+        this.loading = false;
+        this.showHistory = true; 
+      });
+    },
     nextPage: function() {
       this.loading = true;
       this.page_number += 1;
       axios
-      .get(baseUrl + '/api/article?language=eng&page=' + this.page_number + '&dir=next&last_id=' + this.last_id, {withCredentials: true})
+      .get(baseUrl + '/api/article?language=' + this.userData.user_info.language + '&page=' + this.page_number + '&dir=next&last_id=' + this.last_id, {withCredentials: true})
       .then(response => {
         this.info = response.data.contents;
         this.last_id = response.data.last_id;
@@ -62,7 +200,7 @@ new Vue({
       if (this.page_number === 1) 
         this.first_page = true
       axios
-      .get(baseUrl + '/api/article?language=eng&page=' + this.page_number + '&dir=prev&last_id=' + this.last_id, {withCredentials: true})
+      .get(baseUrl + '/api/article?language=' + this.userData.user_info.language + '&page=' + this.page_number + '&dir=prev&last_id=' + this.last_id, {withCredentials: true})
       .then(response => {
         this.info = response.data.contents;
         this.last_id = response.data.last_id;
@@ -70,16 +208,15 @@ new Vue({
       .catch(error => {
         this.errored = true;
       })
-      .finally(() => this.loading = false);    }
-  },
-  filters: {
-    // currencydecimal (value) {
-    //   return value.toFixed(2);
-    // }
-  },
-  mounted () {
-    axios
-      .get(baseUrl + '/api/article?language=eng&page=1', {withCredentials: true})
+      .finally(() => this.loading = false);    
+    },
+    languageSelect: function(iso) {
+      this.loading = true;
+      this.showHistory = false;
+
+      this.userData.user_info.language = iso; // Set newly selected language
+      axios
+      .get(baseUrl + '/api/article?' + this.userData.user_info.language + '&page=1', {withCredentials: true})
       .then(response => {
         this.info = response.data.contents;
         this.last_id = response.data.last_id;
@@ -90,10 +227,47 @@ new Vue({
         this.errored = true;
       })
       .finally(() => this.loading = false);
+    },
+    contentClicked: function(content) {
+      this.modalData.url = content.url;
+      this.modalData.name = content.name;
+      this.showModal = true;
+    }
+  },
+  filters: {
+    // currencydecimal (value) {
+    //   return value.toFixed(2);
+    // }
+  },
+  mounted () {
+    axios
+      .get(baseUrl + '/api/article?page=1', {withCredentials: true})
+      .then(response => {
+        this.info = response.data.contents;
+        this.last_id = response.data.last_id;
+        this.page_number = 1;
+        this.first_page = true;
+
+        this.userData.user_categories = response.data.user_categories;
+        this.userData.user_languages = response.data.user_languages;
+
+        // Set user info
+        this.userData.user_info.username = response.data.user_info.username;
+        this.userData.user_info.email = response.data.user_info.email;    
+        this.userData.user_info.language = response.data.user_info.language;      // Default language     
+        this.userData.user_info.category = response.data.user_info.category;      // Default category
+      })
+      .catch(error => {
+        this.errored = true;
+      })
+      .finally(() => this.loading = false);
 
     // Handle emitters
     this.$root.$on('next', this.nextPage);
     this.$root.$on('prev', this.prevPage);
+    this.$root.$on('languageSelected', this.languageSelect);
+    this.$root.$on('categorySelected', this.categorySelect);
+    this.$root.$on('historySelected', this.getHistory);
   }
 });
   
