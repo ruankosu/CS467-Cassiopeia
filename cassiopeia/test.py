41211bd0f74cca2f91ee8195c8db9a68fe1cced5
carolflyjs <jinsh@oregonstate.edu>
2018-08-10 22:53:53 -0700

    removing the test file

--- cassiopeia/test.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-proj_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-sys.path.insert(0, proj_dir)
-
-from nlp import level_assignment, nlp_training
-
-#level_assignment.assign_levels()
-#nlp_training.refresh_content_level(39)
-nlp_training.refresh_user_level(39)
